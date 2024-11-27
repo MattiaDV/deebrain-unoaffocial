3da25f260c0114eb6fed4e80c854ba7f976d28db
MattiaDV <mattiadv25105@gmail.com>
2024-11-27 18:10:13 +0100

    Aggiornamento server

--- server.mjs
+++ server.mjs
@@ -91,6 +91,124 @@ function getPlatformFromDb(params) {
     });
 }
 
+function getNewAgencyFromDB(params) {
+    return new Promise((resolve, reject) => {
+        connectionDb.get('users_model', params, async (err, partners) => {
+            if (err) {
+                reject("Errore nella ricerca delle location: " + JSON.stringify(err));
+            } else {
+                try {
+                    const enrichedPartners = await Promise.all(
+                        partners.map(async (partner) => {
+                            const locationIds = partner.locations; 
+
+                            const locations = await new Promise((res, rej) => {
+                                connectionDb.get('location_listing', locationIds, (err, locationData) => {
+                                    if (err) rej(err);
+                                    else res(locationData);
+                                });
+                            });
+
+                            const mainS = partner.mainServices; 
+
+                            const mainSS = await new Promise((res, rej) => {
+                                connectionDb.get('main_services', mainS, (err, mainService) => {
+                                    if (err) rej(err);
+                                    else res(mainService);
+                                });
+                            });
+
+                            const disS = partner.mainServices; 
+
+                            const disSS = await new Promise((res, rej) => {
+                                connectionDb.get('main_services', disS, (err, distinctiveService) => {
+                                    if (err) rej(err);
+                                    else res(distinctiveService);
+                                });
+                            });
+
+                            const mediaS = partner.managedMedia; 
+
+                            const mediaSS = await new Promise((res, rej) => {
+                                connectionDb.get('managed_media', mediaS, (err, medias) => {
+                                    if (err) rej(err);
+                                    else res(medias);
+                                });
+                            });
+
+                            const platformS = partner.managedPlatform; 
+
+                            const platformSS = await new Promise((res, rej) => {
+                                connectionDb.get('managed_platform', platformS, (err, platforms) => {
+                                    if (err) rej(err);
+                                    else res(platforms);
+                                });
+                            });
+
+                            return {
+                                ...partner,
+                                locations: locations.map(loc => loc.name),
+                                mainSS: mainSS.map(mains => mains.name),
+                                disSS: disSS.map(dist => dist.name),
+                                mediaSS: mediaSS.map(med => med.name),
+                                platformSS: platformSS.map(plat => plat.name)
+                            };
+                        })
+                    );
+
+                    // Genera le opzioni HTML
+                    const locationOptions = enrichedPartners
+                    .filter(partner => partner.name !== false)
+                    .map(partner => `
+                        <div class="card-account ${partner.agencyType} ${partner.locations.join(' ')} ${partner.mainSS.join(' ')} ${partner.disSS.join(' ')} ${partner.mediaSS.join(' ')} ${partner.platformSS.join(' ')}">
+                            <div class="left-part-card">
+                                <img src="img/logo.png">
+                            </div>
+                            <div class="right-part-card">
+                                <div class="upper-part-card">
+                                    <span class="fs-16 medium-bold-text">
+                                        ${partner.name}
+                                    </span>
+                                    <span class="fs-12 light-text">
+                                        ${partner.agencyType}
+                                    </span>
+                                </div>
+                                <div class="buttonss-listing-infos">
+                                    <div class="location-a">
+                                        <span class="loco">
+                                            <svg fill="#0097b2" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 395.71 395.71" xml:space="preserve" stroke="#0097b2"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M197.849,0C122.131,0,60.531,61.609,60.531,137.329c0,72.887,124.591,243.177,129.896,250.388l4.951,6.738 c0.579,0.792,1.501,1.255,2.471,1.255c0.985,0,1.901-0.463,2.486-1.255l4.948-6.738c5.308-7.211,129.896-177.501,129.896-250.388 C335.179,61.609,273.569,0,197.849,0z M197.849,88.138c27.13,0,49.191,22.062,49.191,49.191c0,27.115-22.062,49.191-49.191,49.191 c-27.114,0-49.191-22.076-49.191-49.191C148.658,110.2,170.734,88.138,197.849,88.138z"></path> </g> </g></svg>${partner.locations.join(', ')} <!-- Elenco nomi delle location -->
+                                        </span>
+                                        <div class="a">Awareness</div>
+                                    </div>
+                                    <ul class="infos-card">
+                                        <li class="light-text fs-12">
+                                            <span class="info-agency number-employee light-text fs-18">${partner.agencyType}</span>
+                                        </li>
+                                        <li class="light-text fs-12">
+                                            <span class="info-agency number-employee light-text fs-18">${partner.numberOfEmployees} employees</span>
+                                        </li>
+                                        <li class="light-text fs-12">
+                                            <span class="info-agency managed-billing light-text fs-18">${partner.managedBilling} Billing</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="bb">
+                                    <button class="fs-18 light-text buttons-style-listing" onclick="window.location.href = 'alkemy.html';">Read more</button>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+
+                    resolve(locationOptions);
+                } catch (err) {
+                    reject("Errore nel recupero delle location: " + JSON.stringify(err));
+                }
+            }
+        });
+    });
+}
+
+
 
 
 
@@ -149,6 +267,23 @@ const server = createServer(async (req, res) => {
 
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(upppp);
+        } catch (err) {
+            console.error(err);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Errore del server interno');
+        }
+        return;
+    }
+
+    if (method === 'GET' && url === '/listing.html') {
+        try {
+            const htmlContent = await readFile('listing.html', 'utf8');
+            const id_cardsAgency = Array.from({ length: 440 }, (_, i) => i);
+            const cardsAgency = await getNewAgencyFromDB(id_cardsAgency);
+            const updatedHtmlContent = htmlContent.replace("{cards}", cardsAgency.join(''));
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(updatedHtmlContent);
         } catch (err) {
             console.error(err);
             res.writeHead(500, { 'Content-Type': 'text/plain' });
