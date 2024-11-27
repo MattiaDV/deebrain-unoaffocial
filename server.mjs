e4e90ce71d80e05db71e23a37a8d9ed2007eac6e
MattiaDV <mattiadv25105@gmail.com>
2024-11-27 14:29:07 +0100

    Correzzione parole

--- server.mjs
+++ server.mjs
@@ -23,7 +23,7 @@ connectionDb.connect(function(err) {
       
     connectionDb.get('location_listing', params_location, function (err, partners) {
         if (err) { 
-            console.error("Errore nella ricerda delle location presenti nel database: " + JSON.stringify(err)); 
+            console.error("Errore nella ricerca delle location presenti nel database: " + JSON.stringify(err)); 
         }
         for (let x = 0; x < partners.length; x++) {
             if (partners[x].name !== false) {
@@ -39,7 +39,7 @@ connectionDb.connect(function(err) {
 
     connectionDb.get('main_services', params_main_service, function(err, main_serv) {
         if (err) {
-            console.error("Errore nella ricerda dei main service presenti sul database: " + JSON.stringify(err));
+            console.error("Errore nella ricerca dei main service presenti sul database: " + JSON.stringify(err));
         }
         for (let x = 0; x < main_serv.length; x++) {
             if (main_serv[x].name !== false) {
@@ -55,7 +55,7 @@ connectionDb.connect(function(err) {
 
     connectionDb.get('distinctive_services', params_distinctive_service, function(err, distinctive_service) {
         if (err) {
-            console.error("Errore nella ricerda dei distinctive service presenti sul database: " + JSON.stringify(err));
+            console.error("Errore nella ricerca dei distinctive service presenti sul database: " + JSON.stringify(err));
         }
         for (let x = 0; x < distinctive_service.length; x++) {
             if (distinctive_service[x].name !== false) {
@@ -71,7 +71,7 @@ connectionDb.connect(function(err) {
 
     connectionDb.get('managed_media', params_managed_media, function(err, managed_media) {
         if (err) {
-            console.error("Errore nella ricerda dei managed media presenti sul database: " + JSON.stringify(err));
+            console.error("Errore nella ricerca dei managed media presenti sul database: " + JSON.stringify(err));
         }
         for (let x = 0; x < managed_media.length; x++) {
             if (managed_media[x].name !== false) {
@@ -87,7 +87,7 @@ connectionDb.connect(function(err) {
 
     connectionDb.get('managed_platform', params_managed_platform, function(err, managed_platform) {
         if (err) {
-            console.error("Errore nella ricerda dei managed platform presenti sul database: " + JSON.stringify(err));
+            console.error("Errore nella ricerca dei managed platform presenti sul database: " + JSON.stringify(err));
         }
         for (let x = 0; x < managed_platform.length; x++) {
             if (managed_platform[x].name !== false) {
