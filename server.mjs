04e475c213894d9f6c3bc21424de917072bbd55a
MattiaDV <mattiadv25105@gmail.com>
2024-11-29 10:59:43 +0100

    Aggiunta managed platform nella pagina personale

--- server.mjs
+++ server.mjs
@@ -439,6 +439,7 @@ const server = createServer(async (req, res) => {
             const mainServiceLoad = await getNormalMainFromDb(id_cardsAgency);
             const distinctiveServiceLoad = await getNormalDisFromDb(id_cardsAgency);
             const managedMediaLoad = await getNormalMediaFromDb(id_cardsAgency);
+            const managedPlatformLoad = await getNormalPlatformFromDb(id_cardsAgency);
 
             const idFounder = nameOfAgency
                 .filter(partner => partner.name !== false)
@@ -465,6 +466,11 @@ const server = createServer(async (req, res) => {
                 .filter(partner => partner.email == emailFromCookie)
                 .map(partner => partner.managedMedia)
 
+            const idPlatform = nameOfAgency
+                .filter(partner => partner.name !== false)
+                .filter(partner => partner.email == emailFromCookie)
+                .map(partner => partner.managedPlatform)
+
             // console.log(idLocation);
             // console.log(idFounder);
 
@@ -473,6 +479,7 @@ const server = createServer(async (req, res) => {
             const idMainServFlat = idMainServ.flat();
             const idDisServFlat = idDisServ.flat();
             const idMediaFlat = idMedia.flat();
+            const idPlatformFlat = idPlatform.flat();
 
             // console.log(idLocationFlat);
 
@@ -499,6 +506,12 @@ const server = createServer(async (req, res) => {
             const realMedia = managedMediaLoad
                 .filter(partner => idMediaFlat.includes(partner.id))
                 .map(partner => partner.name)
+
+            const realPlatform = managedPlatformLoad
+                .filter(partner => idPlatformFlat.includes(partner.id))
+                .map(partner => partner.name)
+
+            console.log(realPlatform);
 
             // console.log(realLocation);
 
@@ -563,11 +576,35 @@ const server = createServer(async (req, res) => {
                     </div>`
                 ).join('')
             )
+            const managedPlatform = mediaManaged.replace('{platformManaged}', realPlatform
+                .map(partner =>
+                    `<div class = "card-platform-m">
+                        <div class = "container-svg">
+                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+                                <path fill="#fbc02d" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#e53935" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4caf50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1565c0" d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+                            </svg>
+                        </div>
+                        <div class = "box-card-platform-managed">
+                            <span class = "name-card fs-16 normal-text">
+                                ${partner}
+                            </span>
+                            <div class = "box-words-platform-managed">
+                                <span class = "certification fs-16 light-text">
+                                    CERTIFICATED
+                                </span>
+                                <span class = "infos fs-16 light-text">
+                                    ${partner}
+                                </span>
+                            </div>
+                        </div>
+                    </div>`
+                ).join('')
+            )
             // console.log(nameOfAgency);
             // console.log(emailFromCookie)
 
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(mediaManaged);
+            res.end(managedPlatform);
         } catch (err) {
             console.error(err);
             res.writeHead(500, { 'Content-Type': 'text/plain' });
